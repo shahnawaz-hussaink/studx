1851536cacba56f3eb09ba0d92495f3b897412cd
Shahnawaz Hussain <shahnawaz.hussain96508@gmail.com>
2025-07-22 20:27:04 +0530

    Added Category Data file in data

---
+++ client/src/Data/categoryData.js
@@ -0,0 +1,14 @@
+
+const  categoryData = [
+        { name: "Phones", image: "/CategoryImages/Mobile.png" },
+        { name: "Laptops", image: "/CategoryImages/Laptop.png" },
+        { name: "Chairs", image: "/CategoryImages/Chair.png" },
+        { name: "Study Tables", image: "/CategoryImages/StudyTable.png" },
+        { name: "Utensils", image: "/CategoryImages/Utensils.png" },
+        { name: "Books", image: "/CategoryImages/Books.png" },
+        { name: "Headphones", image: "/CategoryImages/Headphones.png" },
+        { name: "Gaming", image: "/CategoryImages/Gaming.png" },
+        { name: "Fitness", image: "/CategoryImages/Fitness.png" },
+        { name: "Chargers", image: "/CategoryImages/Chargers.png" },
+    ];
+export default categoryData;
